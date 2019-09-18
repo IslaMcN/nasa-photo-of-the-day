774f81289b9bc96a3ac8bc0b52ec6e89c7c9a30f
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-09-18 12:25:36 -0700

    hi

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import axios from "axios";
 import { Grid, Form, Input, TextArea, Button, Select } from "semantic-ui-react";
 import styled from 'styled-components';
+import PreviousImg from './carousel.js';
 
 const StyledContainer =styled.div`
   width: 100%;
@@ -68,38 +69,7 @@ useEffect(() => {
       
       <StyledP>{info}</StyledP>
       </StyledDiv>
-      <div class = "container">
-        <h2>Past APODs</h2>
-        <div id = "myCarousel">
-        <ol class = "carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to = "0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-      <div className ="carousel-inner">
-        <div class="item active">
-          <img>
-          </img>
-        </div>
-        <div class="item">
-          <img>
-          </img>
-        </div>
-        <div class="item">
-          <img>
-          </img>
-        </div>
-      </div>
-      <a class= "left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class= "right carousel-control" href="#myCarousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only">Next</span>
-      </a>
-      </div>
-      </div>
+      
     </StyledContainer>
   );
 }
--- src/carousel.js
+++ src/carousel.js
@@ -1,30 +1,74 @@
+import React from "react";
+import App from "./App.js";
+import {useEffect, useState} from 'react';
+import Carousel from "bootstrap";
+import axios from 'axios';
+import styled from 'styled-components';
+
 const Container = styled.div``;
 const Title = styled.h2``;
-const Carousel = styled.div``;
 const Ordered = styled.ol``;
 
 const PreviousImg = props => {
-    const [endDate, setEndDate] = useState('');
-    const [startDate, setStartDate] = useState('');
-    const [dateRange, setDateRange] = useState([]);
+    const [img, setImg] = useState()
 
+   
     useEffect(() => {
-        let end = moment(endDate, 'YYYY-MM-DD');
-        let start = moment(startDate, 'YYYY-MM-DD');
-        let dates = [end.format('YYYY-MM-DD')];
-
-        while (end > start) {
-            dates.push(end.subtract(1,'day').format('YYYY-MM-DD'));
-
-        }
-        setDateRange(dates);
-    }, [startDate, endDate]);
-    const updateStartDate = date => {
-        setStartDate(date);
-
-    };
-    const updateEndDate = date => {
-        setEndDate(date);
-    };
-    console.log(start.date);
+        axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5')
+      .then( response => 
+        {const apod = response.data.url;
+        setImg(apod);
+        })
+      .catch(err => console.log('err'));
+      
+      
+      
+        },[] );
+      
+        return (
+        <Container>
+        <Title>Past APODs</Title>
+        <Carousel id = "myCarousel">
+        <Ordered>
+          <li data-target="#myCarousel" data-slide-to = "0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+          <li data-target="#myCarousel" data-slide-to="3"></li>
+          <li data-target="#myCarousel" data-slide-to="4"></li>
+        </Ordered>
+      <div className ="carousel-inner">
+        <div class="item active">
+          <img src = {img} alt ="random apod" />
+          
+        </div>
+        <div class="item">
+        <img src = {img} alt ="random apod" />
+        </div>
+        <div class="item">
+        <img src = {img} alt ="random apod" />
+        
+        </div>
+        <div class="item">
+        <img src = {img} alt ="random apod" />
+        </div>
+        <div class="item">
+        <img src = {img} alt ="random apod" />
+        </div>
+      </div>
+      <a class= "left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class= "right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
+      </Carousel>
+      </Container>
+    )
+    
 }
+
+
+export default PreviousImg;
